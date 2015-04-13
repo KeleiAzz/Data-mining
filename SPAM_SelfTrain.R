8d8cb8a9633aabf51c9dc78c1b3b53eae3ae412e
KeleiAzz <kgong@ncsu.edu>
2015-04-13 15:17:20 -0400

    test PCA

--- SPAM_SelfTrain.R
+++ SPAM_SelfTrain.R
@@ -7,8 +7,8 @@ dataset <- read.csv("data/spambase/data.csv",header=FALSE,sep=";")
 names <- read.csv("data/spambase/names.csv",header=FALSE,sep=";")
 names(dataset) <- sapply((1:nrow(names)),function(i) toString(names[i,1]))
 dataset$y <- as.factor(dataset$y)
-sample <- dataset[sample(nrow(dataset), 1000),]
-trainIndex <- createDataPartition(sample$y, p = .8, list = FALSE, times = 1)
+sample <- dataset[sample(nrow(dataset), 4000),]
+trainIndex <- createDataPartition(sample$y, p = .9, list = FALSE, times = 1)
 dataTrain <- sample[ trainIndex,]
 dataTest  <- sample[-trainIndex,]
 
@@ -18,7 +18,7 @@ confusion(predict(nb,dataTest),dataTest$y)
 
 # Self Train using Bayes
 trST <- dataTrain
-nas <- sample(800,750)
+nas <- sample(3600,3500)
 trST[nas,'y'] <- NA
 func <- function(m,d) {
   p <- predict(m,d,type='raw')
@@ -36,11 +36,12 @@ confusion(predict(stdTree,dataTest,type='class'),dataTest$y)
 
 # Self Train using Decision tree
 trSelfT <- dataTrain
-nas <- sample(800,750)
+nas <- sample(3600,3500)
 trSelfT[nas,'y'] <- NA
 ## Learn a tree using only the labelled cases and test it
 baseTree <- rpartXse(y~ .,trSelfT[-nas,],se=0.5)
 confusion(predict(baseTree,dataTest,type='class'),dataTest$y)
+confusion(predict(baseTree,trSelfT[nas,-58],type='class'),dataTrain[nas,]$y)
 ## The user-defined function that will be used in the self-training process
 
 f <- function(m,d) {
