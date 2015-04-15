1b4638836f770ad69cd7c1b43d85097b623774cc
KeleiAzz <kgong@ncsu.edu>
2015-04-14 21:15:43 -0400

    complete drawing ROC curve

--- ROC.R
+++ ROC.R
@@ -15,11 +15,14 @@ test <- dataset[samples[801:1000],]
 ###################################
 dt <- function(train,test){
   data <- train
-  model <- rpart(y~., data)
+  model <- rpartXse(y~., data,se=0.2)
   preds <- predict(model,test,type='prob')
   return(preds)
 }
-dt(train,test)
+dt.res <- dt(train,test)
+dt.pred <- prediction( dt.res[,2], test[,58] )
+dt.perf <- performance(dt.pred,'tpr','fpr')
+#plot(dt.perf)
 
 ###################################
 #### Navie Bayes 
@@ -33,8 +36,10 @@ nb <- function(train,test) {
   preds <- predict(model,test,type='raw')
   return(preds)
 }
-#nb(train,test)
-
+nb.res <- nb(train,test)
+nb.pred <- prediction( nb.res[,2], test[,58] )
+nb.perf <- performance(nb.pred,'tpr','fpr')
+#plot(nb.perf)
 
 ###################################
 #### Navie Bayes with SMOTE
@@ -47,8 +52,26 @@ nb.s <- function(train,test) {
   preds <- predict(model,test,type='raw')
   return(preds)
 }
-nb.s(train,test)
-
+nb.s.res <- nb.s(train,test)
+nb.s.pred <- prediction( nb.s.res[,2], test[,58] )
+nb.s.perf <- performance(nb.s.pred,'tpr','fpr')
+#plot(nb.s.perf)
+##################################
+#### Ada Boost
+##################################
+ab <- function(train,test) {
+  require(RWeka,quietly=T)
+  data <- train
+  model <- AdaBoostM1(y ~ .,data,
+                      control=Weka_control(I=100))
+  preds <- predict(model,test,
+                   type='probability')
+  return(preds)
+}
+ab.res <- ab(train,test)
+ab.pred <- prediction( ab.res[,2], test[,58] )
+ab.perf <- performance(ab.pred,'tpr','fpr')
+#plot(ab.perf)
 
 ###################################
 #### Self Train with naive bayes
@@ -69,8 +92,34 @@ nb.st <- function(train,test) {
   preds <- predict(model,test,type='raw')
   return(preds)
 }
-nb.st(train,test)
+nb.st.res <- nb.st(train,test)
+nb.st.pred <- prediction( nb.st.res[,2], test[,58] )
+nb.st.perf <- performance(nb.st.pred,'tpr','fpr')
+#plot(nb.st.perf)
 
+###################################
+#### Self Train with Decision tree
+##################################
+pred.dt <- function(m,d) {
+  p <- predict(m,d,type='prob')
+  data.frame(cl=colnames(p)[apply(p,1,which.max)],
+             p=apply(p,1,max)
+  )
+}
+dt.st <- function(train,test) {
+  #require(e1071,quietly=T)
+  train <- train
+  #train[which(train$Insp == 'unkn'),'Insp'] <- NA
+  #train$Insp <- factor(train$Insp,levels=c('ok','fraud'))
+  model <- SelfTrain(y ~ .,train,
+                     learner('rpartXse',list(se=0.2)),'pred.dt')
+  preds <- predict(model,test,type='prob')
+  return(preds)
+}
+dt.st.res <- dt.st(train,test)
+dt.st.pred <- prediction( dt.st.res[,2], test[,58] )
+dt.st.perf <- performance(dt.st.pred,'tpr','fpr')
+#plot(dt.st.perf)
 ###################################
 #### Self Train with AdaBoost
 ##################################
@@ -92,4 +141,25 @@ ab.st <- function(train,test) {
   preds <- predict(model,test,type='probability')
   return(preds)
 }
-ab.st(train,test)
+ab.st.res <- ab.st(train,test)
+ab.st.pred <- prediction( ab.st.res[,2], test[,58] )
+ab.st.perf <- performance(ab.st.pred,'tpr','fpr')
+#plot(ab.st.perf)
+
+#################################
+### ROC curve
+#################################
+plot(dt.perf,col='red')
+par(new=TRUE)
+plot(nb.perf,col='green')
+par(new=TRUE)
+plot(nb.s.perf,col='gray')
+par(new=TRUE)
+plot(ab.perf,col='blue')
+par(new=TRUE)
+plot(nb.st.perf,col='pink')
+par(new=TRUE)
+plot(dt.st.perf,col='yellow')
+par(new=TRUE)
+plot(ab.st.perf)
+
