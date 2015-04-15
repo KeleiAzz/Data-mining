0312a29fbbb51bf8fabf0593c1864d76da2a396e
KeleiAzz <kgong@ncsu.edu>
2015-04-14 20:57:11 -0400

    Finish the function for different classifier methods

---
+++ ROC.R
@@ -0,0 +1,95 @@
+###################################################
+### Data set
+###################################################
+dataset <- read.csv("data/spambase/data.csv",header=FALSE,sep=";")
+names <- read.csv("data/spambase/names.csv",header=FALSE,sep=";")
+names(dataset) <- sapply((1:nrow(names)),function(i) toString(names[i,1]))
+dataset$y <- as.factor(dataset$y)
+
+samples <- sample(4601,1000)
+train <- dataset[samples[1:800],]
+test <- dataset[samples[801:1000],]
+
+###################################
+#### Decistion tree
+###################################
+dt <- function(train,test){
+  data <- train
+  model <- rpart(y~., data)
+  preds <- predict(model,test,type='prob')
+  return(preds)
+}
+dt(train,test)
+
+###################################
+#### Navie Bayes 
+##################################
+nb <- function(train,test) {
+  require(e1071,quietly=T)
+  #sup <- train
+  data <- train
+  #data$y <- factor(data$y,levels=c('ok','fraud'))
+  model <- naiveBayes(y ~ .,data)
+  preds <- predict(model,test,type='raw')
+  return(preds)
+}
+#nb(train,test)
+
+
+###################################
+#### Navie Bayes with SMOTE
+##################################
+nb.s <- function(train,test) {
+  require(e1071,quietly=T)
+  data <- train
+  newData <- SMOTE(y ~ .,data,perc.over=700)
+  model <- naiveBayes(y ~ .,newData)
+  preds <- predict(model,test,type='raw')
+  return(preds)
+}
+nb.s(train,test)
+
+
+###################################
+#### Self Train with naive bayes
+##################################
+pred.nb <- function(m,d) {
+  p <- predict(m,d,type='raw')
+  data.frame(cl=colnames(p)[apply(p,1,which.max)],
+             p=apply(p,1,max)
+  )
+}
+nb.st <- function(train,test) {
+  require(e1071,quietly=T)
+  train <- train
+  #train[which(train$Insp == 'unkn'),'Insp'] <- NA
+  #train$Insp <- factor(train$Insp,levels=c('ok','fraud'))
+  model <- SelfTrain(y ~ .,train,
+                     learner('naiveBayes',list()),'pred.nb')
+  preds <- predict(model,test,type='raw')
+  return(preds)
+}
+nb.st(train,test)
+
+###################################
+#### Self Train with AdaBoost
+##################################
+pred.ada <- function(m,d) {
+  p <- predict(m,d,type='probability')
+  data.frame(cl=colnames(p)[apply(p,1,which.max)],
+             p=apply(p,1,max)
+  )
+}
+ab.st <- function(train,test) {
+  require(RWeka,quietly=T)
+  train <- train
+  #train[which(train$Insp == 'unkn'),'Insp'] <- NA
+  #train$Insp <- factor(train$Insp,levels=c('ok','fraud'))
+  model <- SelfTrain(y ~ .,train,
+                     learner('AdaBoostM1',
+                             list(control=Weka_control(I=100))),
+                     'pred.ada')
+  preds <- predict(model,test,type='probability')
+  return(preds)
+}
+ab.st(train,test)
